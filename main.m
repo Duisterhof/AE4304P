84aef4ac0981021adae20cc58e17d6b17cd1aafd
Bart <b.p.duisterhof@student.tudelft.nl>
2019-03-18 20:32:28 +0100

    updated fft.m routine implementation

--- main.m
+++ main.m
@@ -17,23 +17,23 @@ end
 %% INPUT TURBULENCE- AND AIRCRAFT PARAMETERS
 % AIRCRAFT FLIGHT CONDITION 'LANDING'.
 V     = 51.4;
-m     = 4556;
+m     = 4557;
 twmuc = 2*76;
 KY2   = 0.980;
 c     = 2.022;
 S     = 24.2;
 lh    = 5.5; %horizontal tail length
 g = 9.80665;
-Kt = -0.117;
+Kt = -0.12868;
 
 % TURBULENCE PARAMETERS
 sigma = 1;
-Lg    = 1500;
+Lg    = 150;
 
 sigmaug_V = sigma/V;
 sigmaag   = sigma/V;
 
-% AIRCRAFT SYMMETRIC AERODYNAMIC DERIVATIVES : 
+%%%AIRCRAFT SYMMETRIC AERODYNAMIC DERIVATIVES : 
 CX0 = 0.0000;     CZ0  =-1.1360;     Cm0  =  0.0000;
 CXu =-0.2173;     CZu  =-2.2720;     Cmu  =  0.0000;
 CXa = 0.4692;     CZa  =-5.13;       Cma  = -0.400;
@@ -42,6 +42,58 @@ CXd = 0.0000;     CZd  =-0.6238;     Cmd  = -1.5530;
 CXfa= 0.0000;     CZfa =-1.4050;     Cmfa = -3.615;
                   CZfug= 0.0000;     Cmfug= -Cm0*lh/c;
                   CZfag= CZfa-CZq;   Cmfag=  Cmfa-Cmq;
+%%% JOSE PARAMETERS
+%sigmaug_V = sigma/V;
+%sigmaag   = sigma/V;
+%
+% W = 32000;         % N
+% S = 25.95;         % m^2
+% c = 1.98;          % m
+% x_cg = 0.236*c;    % m
+% V = 46;            % m/s
+% lh = 4.85;        % m
+% twmuc = 106.04;    % -
+% m =W/g;
+% twmucKYtw = 66.16; % -
+% KY2   = twmucKYtw/twmuc;
+% 
+% sigmaug_V = sigma/V;
+% sigmaag   = sigma/V;
+% 
+% g = 9.80665;       % m/s^2
+% 
+% CX0 = 0;
+% CXu = -0.27;
+% CXa = 0.45;
+% CXfag = 0;
+% CXq = 0;
+% CXd = 0;
+% CXug = CXu;
+% CXfug = 0;
+% CXag = CXa;
+% CXfag = 0;
+% 
+% CZ0 = -0.98;
+% CZu = -1.95;
+% CZa = -5.23;
+% CZfa = -1.43;
+% CZq = -3.51;
+% CZd = -0.33;
+% CZug = CZu;
+% CZfug = 0; %%%%
+% CZag = CZa;    
+% CZfag = CZfa - CZq;
+% 
+% Cm0 = 0;
+% Cmu = -0.04;
+% Cma = -0.44;
+% Cmfa = -3.55;
+% Cmq = -5.2;
+% Cmd = -0.99;
+% Cmug = Cmu;
+% Cmfug = -Cm0 * lh/c; %%%%
+% Cmag = Cma;    
+% Cmfag = Cmfa - Cmq
 
 % CALCULATION OF AIRCRAFT SYMMETRIC STABILITY DERIVATIVES
 xu   = (V/c)*(CXu/twmuc);
@@ -97,8 +149,8 @@ B=...
 n_z_row = (V/g)*(A(3,:)-A(2,:));
 C = [eye(4) zeros(4,3);
 n_z_row]; 
-D = zeros(5,3);
-
+D = [zeros(4,3);
+    (V/g)*(B(3,:)-B(2,:))];
 if n==0
     %TF's for gain determination
     sys_no_pd = ss(A,B,C,D);
@@ -117,7 +169,6 @@ end
 %Gains obtained through root-locus method
 Ka = 0;
 Kq = 0;
-Kt = -0.117;
 K = [0 Ka Kt Kq 0 0 0 ];
 
 %if pitchdamer is enabled, modify A-matrix
@@ -131,9 +182,10 @@ switch n
         %% Time-domain analysis
 
         % TIME AXIS INPUT VECTOR DEFINITION
+        rng(1)
         dt = 0.01;
         T  = 60; 
-        t = [0:dt:T];
+        t = 0:dt:T;
         N = length(t);
 
         % INPUT VECTOR DEFINITION
@@ -150,27 +202,27 @@ switch n
         a_z_ref = diff(y(:,3)-y(:,2))*V/(g*dt);
 
         % PLOTTING RESULTS
-        subplot(2,3,1);
-        plot(t,y(:,1)*180/pi)
+        subplot(6,1,1);
+        plot(t,y(:,1))
         xlabel('time [s]'); ylabel('u/V [-]'); title('airspeed deviation');
 
-        subplot(2,3,2);
+        subplot(6,1,2);
         plot(t,y(:,2)*180/pi)
         xlabel('time [s]'); ylabel('alpha [deg]'); title('angle of attack');
 
-        subplot(2,3,3);
+        subplot(6,1,3);
         plot(t,y(:,3)*180/pi)
         xlabel('time [s]'); ylabel('theta [deg]'); title('pitch angle');
 
-        subplot(2,3,4);
+        subplot(6,1,4);
         plot(t,y(:,4)*180/pi)
         xlabel('time [s]'); ylabel('qc/V [deg]'); title('pitch rate');
 
-        subplot(2,3,5);
+        subplot(6,1,5);
         plot(t,y(:,5))
         xlabel('time [s]'); ylabel('load_factor'); title('Load factor from state space');
 
-        subplot(2,3,6);
+        subplot(6,1,6);
         N = length(t);
         plot( t(:,1:N-1),a_z_ref(1:N-1))
         xlabel('time [s]'); ylabel('load_factor'); title('Load factor from time signals');
@@ -179,8 +231,8 @@ switch n
         %% Time-domain signals to be used
 
         % TIME AXIS INPUT VECTOR DEFINITION
-        dt = 0.01;
-        T  = 300; 
+        dt = 0.05;
+        T  = 200; 
         t = [0:dt:T];
         N = length(t);
         Fs = 1/dt;
@@ -231,13 +283,11 @@ switch n
         subplot(2,3,5); 
         loglog(omega,Snn); xlabel('omega [rad/sec]'); ylabel('Snn [rad^2]');
 
-        
         %% experimental method, fft.m
 
         X = [y(:,1),y(:,2),y(:,3),y(:,4),y(:,5)];
-        mag = fft(X);
+        mag = dt*fft(X);
         
-        %double-sided
         Suu = mag(1:N/2+1,1);
         Saa = mag(1:N/2+1,2);
         Stt = mag(1:N/2+1,3);
@@ -245,21 +295,14 @@ switch n
         Snn = mag(1:N/2+1,5);
 
         %convert to PSD
-        Suu = (1/(N*Fs))*abs(Suu).^2;
-        Saa = (1/(N*Fs))*abs(Saa).^2;
-        Stt = (1/(N*Fs))*abs(Stt).^2;
-        Sqq = (1/(N*Fs))*abs(Sqq).^2;
-        Snn = (1/(N*Fs))*abs(Snn).^2;
+        Suu = (1/T)*abs(Suu).^2;
+        Saa = (1/T)*abs(Saa).^2;
+        Stt = (1/T)*abs(Stt).^2;
+        Sqq = (1/T)*abs(Sqq).^2;
+        Snn = (1/T)*abs(Snn).^2;
         
-        %convert to single sided
-        Suu(2:end-1) = (2*Suu(2:end-1));
-        Saa(2:end-1) = (2*Saa(2:end-1));
-        Stt(2:end-1) = (2*Stt(2:end-1));
-        Sqq(2:end-1) = (2*Sqq(2:end-1));
-        Snn(2:end-1) = (2*Snn(2:end-1));
-
-        Fs = N/T;
-        f = (Fs/N)*(0:(N/2));
+        f = 2*pi*Fs*(0:(N/2))/N;          %frequency array in rad/s
+        N = length(f);
 
         % PLOT POWER SPECTRA
         figure(2)
@@ -273,16 +316,21 @@ switch n
         loglog(f,Sqq); xlabel('omega [rad/sec]'); ylabel('Sqq [rad^2]');
         subplot(2,3,5); 
         loglog(f,Snn); xlabel('omega [rad/sec]'); ylabel('Snn [rad^2]');
-
+        
+ 
         %% Experimental PSD, pwelch.m
         X = [y(:,1),y(:,2),y(:,3),y(:,4),y(:,5)];
+        f = (Fs/N)*(0:(N/2)); %frequency input array in Hz
         [pxx,f] = pwelch(X,[],[],f,Fs);
-        Suu = 2*pxx(:,1);
-        Saa = 2*pxx(:,2);
-        Stt = 2*pxx(:,3);
-        Sqq = 2*pxx(:,4);
-        Snn = 2*pxx(:,5);
-
+        
+        %converting everything to rad
+        f = f*2*pi;
+        Suu = pxx(:,1);
+        Saa = pxx(:,2);
+        Stt = pxx(:,3);
+        Sqq = pxx(:,4);
+        Snn = pxx(:,5);
+                
         % PLOT POWER SPECTRA
         figure(3)
         subplot(2,3,1); 
@@ -301,7 +349,7 @@ switch n
 
         % TIME AXIS INPUT VECTOR DEFINITION
         dt = 0.01;
-        T  = 300; 
+        T  = 30000; 
         t = [0:dt:T];
         N = length(t);
         Fs = N/T;
@@ -326,7 +374,8 @@ switch n
         L   = L(1:4,1:4);   
         var_arr = [L(1,1),L(2,2),L(3,3),L(4,4)];
         disp('Using lyapunov');
-        disp(var_arr)
+        disp('1.0e-06 *')
+        disp(10^6*var_arr)
         
         
         %% NUMERICAL INTEGRATION OF PSD's
@@ -357,8 +406,8 @@ switch n
         var_arr(4) = sum(do.*Sqq(1:N-1));    
         var_arr(5) = sum(do.*Snn(1:N-1));
         disp('Integrating analytical PSDs');
-        disp('1.0e-03 *')
-        disp(10^3*var_arr/pi)
+        disp('1.0e-06 *')
+        disp(10^6*var_arr/pi)
                
 
         
@@ -367,7 +416,7 @@ switch n
         % generate plots with fft
         
         X = [y(:,1),y(:,2),y(:,3),y(:,4),y(:,5)];
-        mag = fft(X);
+        mag = dt*fft(X);
         
         Suu = mag(1:N/2+1,1);
         Saa = mag(1:N/2+1,2);
@@ -376,20 +425,13 @@ switch n
         Snn = mag(1:N/2+1,5);
 
         %convert to PSD
-        Suu = (1/(N*Fs))*abs(Suu).^2;
-        Saa = (1/(N*Fs))*abs(Saa).^2;
-        Stt = (1/(N*Fs))*abs(Stt).^2;
-        Sqq = (1/(N*Fs))*abs(Sqq).^2;
-        Snn = (1/(N*Fs))*abs(Snn).^2;
+        Suu = (1/T)*abs(Suu).^2;
+        Saa = (1/T)*abs(Saa).^2;
+        Stt = (1/T)*abs(Stt).^2;
+        Sqq = (1/T)*abs(Sqq).^2;
+        Snn = (1/T)*abs(Snn).^2;
         
-        %convert to single sided
-        Suu(2:end-1) = (2*Suu(2:end-1));
-        Saa(2:end-1) = (2*Saa(2:end-1));
-        Stt(2:end-1) = (2*Stt(2:end-1));
-        Sqq(2:end-1) = (2*Sqq(2:end-1));
-        Snn(2:end-1) = (2*Snn(2:end-1));
-
-        f = (Fs/N)*(0:(N/2));
+        f = 2*pi*Fs*(0:(N/2))/N;          %frequency array in rad/s
         N = length(f);
         
         do = diff(f)';
@@ -400,29 +442,36 @@ switch n
         var_arr(4) = sum(do.*Sqq(1:N-1));    
         var_arr(5) = sum(do.*Snn(1:N-1));
         disp('Integrating fft.m PSDs:');
-        disp('1.0e-03 *')
-        disp(10^3*var_arr)     
+        disp('1.0e-06 *')
+        disp(10^6*var_arr/pi)     
         
         %% using pwelch.m
+        f = (Fs/N)*(0:(N/2)); %frequency input array in Hz
         [pxx,f] = pwelch(X,[],[],f,Fs);
-        Suu = 2*pxx(:,1);
-        Saa = 2*pxx(:,2);
-        Stt = 2*pxx(:,3);
-        Sqq = 2*pxx(:,4);
-        Snn = 2*pxx(:,5);
+        Suu = pxx(:,1);
+        Saa = pxx(:,2);
+        Stt = pxx(:,3);
+        Sqq = pxx(:,4);
+        Snn = pxx(:,5);
+        
+        f = (Fs/N)*(0:(N/2))*2*pi; %frequency array in rad/s
+        do = diff(f)';
+        N = length(f);
+        
         var_arr(1) = sum(do.*Suu(1:N-1));
         var_arr(2) = sum(do.*Saa(1:N-1));
         var_arr(3) = sum(do.*Stt(1:N-1));
         var_arr(4) = sum(do.*Sqq(1:N-1));    
         var_arr(5) = sum(do.*Snn(1:N-1));
         disp('Integrating pwelch.m PSDs:');
-        disp('1.0e-03 *')
-        disp(10^3*var_arr)
+        disp('1.0e-06 *')
+        disp(10^6*var_arr/pi)
         
         %% using var.m
         var_arr = var(y);
         disp('Using var.m routine:')
-        disp(var_arr)
+        disp('1.0e-06 *')
+        disp(10^6*var_arr)
         
         
     otherwise 
